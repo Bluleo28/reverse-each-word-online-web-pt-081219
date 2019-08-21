ae7c8ac6939a2c68a4f31effe3fa011e98573ac3
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-21 03:40:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,1 @@
-def reverse_each_word(string)  string_array = string.split()  new_string_array = string_array.collect { |word| word.reverse }  new_string_array.join(' ')end
+def reverse_each_word(sentence)  sentence.split.collect {|word| word.reverse}.join(" ")end
